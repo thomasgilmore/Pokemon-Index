a4e4606d32047a73d14d2cafe69b4d8ace19a818
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-02-25 11:17:04 -0500

    moved functions into Home.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
 
-export default function Header({ onSearchSubmit }) {
+export default function Header({ onSearchSubmit, handleInputChange }) {
   return (
     <nav className='header-container'>
       <h1 className='header-container__title'>Favorite Movies</h1>
       <form onSubmit={onSearchSubmit}>
-        <input type="text" className='header-container__input' />
+        <input type="text" onChange={handleInputChange} className='header-container__input' />
       </form>
       <Link className='header-container__login-link' to="login">
         <button className='header-container__login-button'>Login</button>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 
-export default function Home({ onSearchSubmit }) {
+export default function Home() {
+
+  const API_URL = 'https://api.pokemontcg.io/v2'
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value)
+  }
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    fetch(`${API_URL}/cards?q=name:${inputValue}`)
+      .then(res => res.json())
+      .then((result) => {
+        console.log(result);
+      })
+  }
+
+  console.log(inputValue);
   return (
     <div>
-      <Header onSearchSubmit={onSearchSubmit} />
+      <Header onSearchSubmit={onSearchSubmit} handleInputChange={handleInputChange} />
+      <div className='cardDetails'>
+        <div className='card'></div>
+        <div className='detail'></div>
+      </div>
+      <div className='cardList'></div>
     </div>
   )
 }
