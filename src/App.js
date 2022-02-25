33615fe3d1cb08aed48080417d77b5054028d7dd
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-02-24 20:51:51 -0500

    toggleViewLogin created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Header from "./Components/Header";
+import Login from "./Components/Login";
 
 function App() {
+  const [viewLogin, setViewLogin] = useState(false);
+
+  const toggleViewLogin = () => {
+    setViewLogin(!viewLogin);
+  }
+
   return (
     <div>
-      <Header />
+      <Header toggleViewLogin={toggleViewLogin} />
+      {viewLogin ? <Login toggleViewLogin={toggleViewLogin} /> : null }
     </div>
   );
 }
