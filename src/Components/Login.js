97a82c9d5ddb05012061a2acb8f24f7b5965f621
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-02-27 14:59:51 -0500

    Updated to Login.css

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './login.css';
+import './Login.css';
 import GoogleLogin from 'react-google-login';
 
 export default function Login() {
