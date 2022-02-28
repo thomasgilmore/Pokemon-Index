258358b5cf794955c36c3561eadd2cc5bf79975b
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-02-27 23:10:05 -0500

    Pokemon Card

--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './PokemonCard.css';
 
-export default function PokemonCard({ img, name }) {
+export default function PokemonCard({ img, name, cardId, handlePokemonCardChange }) {
   return (
-    <img src={img} alt={name} className="pokemon-card__img" />
+    <button onClick={handlePokemonCardChange} className='pokemon-card__button'><img src={img} alt={name} name={cardId} className="pokemon-card__img" /></button>
   )
 }
