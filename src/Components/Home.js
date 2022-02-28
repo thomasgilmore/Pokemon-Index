576b97b0d15f38d79183fcb3e4c8160102888b3d
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-02-27 23:10:32 -0500

    now you can click on the card and the card to display will change:

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
 import './Home.css';
 import React, { useState } from 'react';
 import Header from './Header';
+import PokemonCardToShow from './PokemonCardToShow';
+import PokemonCardDetails from './PokemonCardDetails';
 import PokemonCard from './PokemonCard';
 
 export default function Home() {
@@ -8,6 +10,9 @@ export default function Home() {
   const API_URL = 'https://api.pokemontcg.io/v2'
 
   const [inputValue, setInputValue] = useState('');
+  const [cardToDisplay, setcardToDisplay] = useState('');
+  const [nameToDisplay, setNameToDisplay] = useState('');
+  const [detailsToDisplay, setDetailsToDisplay] = useState('');
   const [cardList, setCardList] = useState([]);
 
   const handleInputChange = (event) => {
@@ -24,18 +29,35 @@ export default function Home() {
       })
   }
 
+  const handlePokemonCardChange = (event) => {
+    event.preventDefault();
+    console.log(event);
+    let filteredArray = cardList.filter((card) => {
+      return card.id === event.target.name;
+    });
+    console.log(filteredArray);
+    setcardToDisplay(filteredArray[0].images.large);
+    setNameToDisplay(filteredArray[0].name);
+    setDetailsToDisplay(filteredArray[0].attacks);
+  }
+
   console.log(inputValue);
   console.log(cardList);
+  console.log(nameToDisplay);
   return (
     <div>
       <Header onSearchSubmit={onSearchSubmit} handleInputChange={handleInputChange} />
       <div className='cardDetails'>
-        <div className='card'></div>
-        <div className='detail'></div>
+        <div className='card'>
+          {cardList.length > 0 ? <PokemonCardToShow img={cardToDisplay || cardList[0].images.large} title={nameToDisplay || cardList[0].name} /> : null}
+        </div>
+        <div className='detail'>
+          {cardList.length > 0 ? <PokemonCardDetails title={nameToDisplay || cardList[0].name} attacks={detailsToDisplay || cardList[0].attacks} /> : null}
+        </div>
       </div>
       <div className='cardList'>
         {cardList.length > 0 ? cardList.map((card) => {
-          return (<PokemonCard img={card.images.small} name={card.name} key={card.id} />)
+          return (<PokemonCard img={card.images.small} name={card.name} cardId={card.id} key={card.id} handlePokemonCardChange={handlePokemonCardChange} />)
         }) : null}
       </div>
     </div>
