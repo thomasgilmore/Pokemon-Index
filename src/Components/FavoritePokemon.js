ee9dd50e04c8a22e827c3979e24d76f50778e732
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-02-28 21:47:18 -0500

    working on favorite pokemon page

--- src/Components/FavoritePokemon.js
+++ src/Components/FavoritePokemon.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './FavoritePokemon.css';
 import { Link } from 'react-router-dom';
 import PokemonCard from './PokemonCard';
 
 export default function FavoritePokemon({ favoritePokemonCards, handlePokemonCardChange, handlePokemonCardSave }) {
   return (
-    <div>
-      <h1 className=''>Favorite Pokemon</h1>
-      <Link to='/'>Home</Link>
+    <div className='favorite-pokemon-container'>
+      <header className='favorite-pokemon-container__header'>
+        <h1 className='favorite-pokemon__header-title'>Favorite Pokemon</h1>
+        <Link to='/' className='favorite-pokemon__header-home-link'>Home</Link>
+      </header>
       <div className='cardList'>
           {favoritePokemonCards.length > 0 ? favoritePokemonCards.map((card) => {
             return (<PokemonCard img={card.images.small} name={card.name} cardId={card.id} key={card.id} handlePokemonCardChange={handlePokemonCardChange} handlePokemonCardSave={handlePokemonCardSave} />)
