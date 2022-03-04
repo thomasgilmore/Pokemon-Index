da0227f645806a8e2b481ddb64635334611193b5
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-04 15:21:28 -0500

    created UserDashboard.js

---
+++ src/Components/UserDashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserDashboard() {
+  return (
+    <nav className='header-container'>
+      <h1 className='header-container__title'>User Dashboard</h1>
+    </nav>
+  )
+}
