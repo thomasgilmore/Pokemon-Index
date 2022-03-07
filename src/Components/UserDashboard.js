c4e51b2ae72d5ac509036ba258ed6b77c9cbe814
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-07 10:07:29 -0500

    working on userdashboard

--- src/Components/UserDashboard.js
+++ src/Components/UserDashboard.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import './UserDashboard.css';
 
-export default function UserDashboard() {
+export default function UserDashboard({ googleUserData }) {
+  let profileImg = 'loading...';
+  let userName
+  let userEmail
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(googleUserData.profileObj);
+      profileImg = googleUserData.profileObj.imageUrl;
+      userName = googleUserData.profileObj.name;
+      userEmail = googleUserData.profileObj.email;
+    }, 1500)
+  }, [])
+
+  console.log(googleUserData);
   return (
-    <nav className='header-container'>
-      <h1 className='header-container__title'>User Dashboard</h1>
-    </nav>
+      <nav className='user-dashboard__container'>
+        <h1 className='user-dashboard__title'>User Dashboard</h1>
+          <img className='user-dashboard__profile-img' src={profileImg} alt="user image" />
+          <span className='user-dashboard__information'>
+          <span className='user-dashboard__user-name'>{userName}</span>
+          <span className='user-dashboard__user-email'>{userEmail}</span>
+        </span>
+      </nav>
   )
 }
