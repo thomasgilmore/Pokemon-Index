f6f9276fc0bfe7f52e842347b81b5081df810a4e
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-08 15:44:20 -0500

    go back home link and now displays favorite pokemon as well

--- src/Components/UserDashboard.js
+++ src/Components/UserDashboard.js
@@ -1,20 +1,38 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './UserDashboard.css';
+import PokemonCard from './PokemonCard';
 
-export default function UserDashboard({ googleUserData }) {
+export default function UserDashboard({ googleUserData, favoritePokemonCards, handlePokemonCardChange, handlePokemonCardSave, handlePokemonCardDelete }) {
   let profileImg = googleUserData.profileObj.imageUrl;
   let userName = googleUserData.profileObj.name;
   let userEmail = googleUserData.profileObj.email;
 
+  const navigate = useNavigate();
+
+  const handleGoToHome = () => {
+    navigate('/');
+  }
+
   console.log(googleUserData);
   return (
+    <div>
       <nav className='user-dashboard__container'>
         <h1 className='user-dashboard__title'>User Dashboard</h1>
+          <span onClick={handleGoToHome} className='user-dashboard__home-link'>Home</span>
           <img className='user-dashboard__profile-img' src={profileImg} alt="user image" />
           <span className='user-dashboard__information'>
           <span className='user-dashboard__user-name'>{userName}</span>
           <span className='user-dashboard__user-email'>{userEmail}</span>
         </span>
       </nav>
+      <div className='cardList'>
+          {favoritePokemonCards && (
+            favoritePokemonCards.map((card) => 
+            <PokemonCard img={card.images.small} name={card.name} cardId={card.id} key={card.id} handlePokemonCardChange={handlePokemonCardChange} handlePokemonCardSave={handlePokemonCardSave} handlePokemonCardDelete={handlePokemonCardDelete} hideDeleteButton={false} />
+            )
+          )}
+      </div>
+    </div>
   )
 }
