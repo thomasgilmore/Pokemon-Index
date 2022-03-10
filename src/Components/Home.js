62b0565965211753f58fc324a784cbe373e4790c
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-09 21:28:07 -0500

    moved all state and functions into Context.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,164 +1,30 @@
+import React from 'react';
 import './Home.css';
-import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-// import Header from './Header';
-// import PokemonCardToShow from './PokemonCardToShow';
-// import PokemonCardDetails from './PokemonCardDetails';
-// import PokemonCard from './PokemonCard';
-import FavoritePokemon from './FavoritePokemon';
-import HomePage from './HomePage';
-import Login from './Login';
-import UserDashboard from './UserDashboard';
+import Header from './Header';
+import PokemonCardToShow from './PokemonCardToShow';
+import PokemonCardDetails from './PokemonCardDetails';
+import PokemonCard from './PokemonCard';
+import { usePokemonContextProvider } from '../Context/Context';
 
 export default function Home() {
-
-  const API_URL = 'https://api.pokemontcg.io/v2'
-
-  const [inputValue, setInputValue] = useState('');
-  const [cardToDisplay, setcardToDisplay] = useState('');
-  const [nameToDisplay, setNameToDisplay] = useState('');
-  const [detailsToDisplay, setDetailsToDisplay] = useState('');
-  const [cardList, setCardList] = useState([]);
-  const [favoritePokemonCards, setFavoritePokemonCards] = useState([]);
-  const [googleUserData, setGoogleUserData] = useState([]);
-
-  
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value)
-  }
-
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    fetch(`${API_URL}/cards?q=name:${inputValue}`)
-      .then(res => res.json())
-      .then((result) => {
-        setCardList(result.data);
-      })
-  }
-
-  const handlePokemonCardChange = (event) => {
-    event.preventDefault();
-    let filteredArray = cardList.filter((card) => {
-      return card.id === event.target.name;
-    });
-    setcardToDisplay(filteredArray[0].images.large);
-    setNameToDisplay(filteredArray[0].name);
-    setDetailsToDisplay(filteredArray[0].attacks);
-  }
-
-  const handleCheckIfSaved = (pokemonCard) => {
-    const checkSavedArray = favoritePokemonCards.filter((card) => {
-      if (card.id === pokemonCard[0].id) {
-        return  true;
-      } else {
-        return false;
-      }
-    })
-   return checkSavedArray
-  }
-
-  const handleRemovePokemonCard = (pokemonCard) => {
-    let arrayWithRemovedPokemon = favoritePokemonCards.filter((card) => {
-      return card.id !== pokemonCard[0].id;
-    })
-    return arrayWithRemovedPokemon;
-  }
-
-  const handlePokemonCardDelete = (event) => {
-    event.preventDefault();
-    const cardId = event.target.name;
-    let allTheOtherCards = favoritePokemonCards.filter((card) => card.id !== cardId)
-    setFavoritePokemonCards(allTheOtherCards);
-  }
-
-  const handlePokemonCardSave = (event) => {
-    event.preventDefault();
-    const cardId = event.target.name;
-    const isSaved = favoritePokemonCards.some((card) => card.id === cardId)
-    const cardToSave = cardList.find((card) => card.id === cardId)
-    if (!isSaved) {
-      let newFavoriteArr = favoritePokemonCards
-      newFavoriteArr.push(cardToSave)
-      setFavoritePokemonCards(newFavoriteArr)
-    }
-    // console.log(isSaved)
-
-    // console.log(event);
-    // let allPokemonCardsToSave = [];
-    // let pokemonCardToFavorite = cardList.filter((card) => {
-    //   return card.id === event.target.name;
-    // })
-    // if (handleCheckIfSaved(pokemonCardToFavorite).length > 0) {
-    //   allPokemonCardsToSave = handleRemovePokemonCard(pokemonCardToFavorite);
-    // } else {
-    //   allPokemonCardsToSave = [...favoritePokemonCards, ...pokemonCardToFavorite];
-    // }
-    // console.log(allPokemonCardsToSave);
-    // setFavoritePokemonCards(allPokemonCardsToSave);
-  }
-  // const handlePokemonCardSaveNew = (event) => {
-    
-  // }
-  // Fetch on component did mount
-  useEffect(() => {
-    fetch(`${API_URL}/cards`)
-    .then(res => res.json())
-    .then((result) => {
-      setCardList(result.data);
-    })
-  }, []);
-
-  // console.log(inputValue);
-  // console.log(cardList);
-  // console.log(nameToDisplay);
+  const { cardList, nameToDisplay, cardToDisplay, detailsToDisplay } = usePokemonContextProvider()
+  console.log("home", cardList)
   return (
     <div>
-      <Routes>
-        <Route 
-          path="/"
-          element={
-            <HomePage 
-            handleInputChange={handleInputChange} 
-            handleSearchSubmit={handleSearchSubmit} 
-            handlePokemonCardChange={handlePokemonCardChange} 
-            handlePokemonCardSave={handlePokemonCardSave}
-            handlePokemonCardDelete={handlePokemonCardDelete}
-            cardToDisplay={cardToDisplay}
-            detailsToDisplay={detailsToDisplay}
-            cardList={cardList}
-            nameToDisplay={nameToDisplay}
-          />}
-        />
-        <Route 
-          path="favoritepokemon" 
-          element={
-            <FavoritePokemon 
-            favoritePokemonCards={favoritePokemonCards} 
-            handlePokemonCardChange={handlePokemonCardChange} 
-            handlePokemonCardSave={handlePokemonCardSave} 
-            handlePokemonCardDelete={handlePokemonCardDelete}
-          />} 
-        />
-        <Route 
-          path="login" 
-          element={
-            <Login 
-            setGoogleUserData={setGoogleUserData} 
-          />} 
-        />
-        <Route 
-          path="userdashboard" 
-          element={
-            <UserDashboard 
-            googleUserData={googleUserData} 
-            favoritePokemonCards={favoritePokemonCards}
-            handlePokemonCardChange={handlePokemonCardChange} 
-            handlePokemonCardSave={handlePokemonCardSave}
-            handlePokemonCardDelete={handlePokemonCardDelete}
-            />} 
-          />
-      </Routes>
+      <Header />
+      <div className='cardDetails'>
+        <div className='card'>
+          {cardList.length > 0 ? <PokemonCardToShow img={cardToDisplay || cardList[0].images.large} title={nameToDisplay || cardList[0].name} /> : null}
+        </div>
+        <div className='detail'>
+          {cardList.length > 0 ? <PokemonCardDetails title={nameToDisplay || cardList[0].name} attacks={detailsToDisplay || cardList[0].attacks} /> : null}
+        </div>
+      </div>
+      <div className='cardList'>
+        {cardList.length > 0 ? cardList.map((card) => {
+          return (<PokemonCard img={card.images.small} name={card.name} cardId={card.id} key={card.id} hideDeleteButton={true} />)
+        }) : null}
+      </div>
     </div>
   )
 }
