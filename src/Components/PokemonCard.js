f0a5a57bc6b8cfb7cbc45b785db57b66b4d0c420
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-23 21:12:43 -0400

    updated save and delete functions to use local storage saved googleId when signedIn

---
+++ src/Components/PokemonCard.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import './PokemonCard.css';
+import { PokemonContext } from '../Context/Context';
+
+// interface IPokemonCardProps {
+//   img: string;
+//   name: string;
+//   cardId: string;
+//   hideDeleteButton: boolean;
+// }
+
+// export default function PokemonCard({ img, name, cardId, hideDeleteButton }: IPokemonCardProps) {
+export default function PokemonCard({ img, name, cardId, hideDeleteButton }) {
+  const { handlePokemonCardChange, handlePokemonCardSave, handlePokemonCardDelete } = useContext(PokemonContext)
+  return (
+    <span className='pokemon-card-container'>
+    {hideDeleteButton ? 
+      <button className='pokemon-card__save-button' name={cardId} onClick={handlePokemonCardSave}>Save</button>
+    : <button className='pokemon-card__delete-button' name={cardId} onClick={handlePokemonCardDelete}>Delete</button>
+    }
+      <img  className="pokemon-card__img" src={img} alt={name} name={cardId} onClick={handlePokemonCardChange} />
+    </span>
+  )
+}
