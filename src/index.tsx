6f72b8c89e92a736a0901220ca7c204a6943265c
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-28 22:51:04 -0400

    working on making eslint to work

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import App from './App.tsx';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { PokemonContextProvider } from "./Context/Context";
 
