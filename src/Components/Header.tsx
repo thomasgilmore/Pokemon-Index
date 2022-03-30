7f5bd7148805687afa5b9613b562f48646ffc1dd
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-30 14:36:04 -0400

    changed Header.js to typescript

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import './Header.css';
-import { PokemonContext } from '../Context/Context';
+import { PokemonContext } from '../Context/Context.tsx';
 
 export default function Header() {
   const { handleSearchSubmit, handleInputChange, handleSignOut, isSignedInState } = useContext(PokemonContext)
@@ -25,18 +25,18 @@ export default function Header() {
       <form onSubmit={handleSearchSubmit}>
         <input type="text" onChange={handleInputChange} className='header-container__input' />
       </form>
-      {isSignedInState ? 
+      {isSignedInState ?
         <React.Fragment>
-        <span onClick={handleGoToUserDashboard} className='header-container__user-dashboard-link'>User Dashboard</span> 
-        <button onClick={handleSignOut} className='header-container__sign-out-button'>Sign Out</button>
+          <span onClick={handleGoToUserDashboard} className='header-container__user-dashboard-link'>User Dashboard</span>
+          <button onClick={handleSignOut} className='header-container__sign-out-button'>Sign Out</button>
         </React.Fragment>
-      : 
-      <React.Fragment>
-        <span onClick={handleGoToFavoritePokemonPage} className='header-container__favorite-pokemon-link'>Favorite Pokemon</span>
-        <button onClick={handleGoToLoginPage} className='header-container__login-button'>Login</button>
-        <button className='header-container__register-button'>Register</button>
-      </React.Fragment> 
-    }
+        :
+        <React.Fragment>
+          <span onClick={handleGoToFavoritePokemonPage} className='header-container__favorite-pokemon-link'>Favorite Pokemon</span>
+          <button onClick={handleGoToLoginPage} className='header-container__login-button'>Login</button>
+          <button className='header-container__register-button'>Register</button>
+        </React.Fragment>
+      }
     </nav>
   )
 }
