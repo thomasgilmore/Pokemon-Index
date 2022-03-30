47a95dd21a3ff7ae3e887aceae9e0f38782f1863
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-30 14:33:39 -0400

    changed app.js to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./Components/Home";
-import FavoritePokemon from "./Components/FavoritePokemon";
-import Login from "./Components/Login";
-import UserDashboard from "./Components/UserDashboard";
+import Home from "./Components/Home.tsx";
+import FavoritePokemon from "./Components/FavoritePokemon.tsx";
+import Login from "./Components/Login.tsx";
+import UserDashboard from "./Components/UserDashboard.tsx";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="*"  element={<Home />} />
+        <Route path="*" element={<Home />} />
         <Route path="favoritepokemon" element={<FavoritePokemon />} />
         <Route path="login" element={<Login />} />
         <Route path="userdashboard" element={<UserDashboard />} />
