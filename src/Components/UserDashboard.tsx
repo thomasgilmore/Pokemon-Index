f9405fcb9c90469f75420ebe755decbd6221b568
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-30 18:40:36 -0400

    fixing errors:

--- src/Components/UserDashboard.tsx
+++ src/Components/UserDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './UserDashboard.css';
 import PokemonCard from './PokemonCard.tsx';
