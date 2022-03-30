35d21576a26479a6c0f5a0956fb59f825d7a8ece
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-30 14:34:02 -0400

    changed userdashboard to typescript

--- src/Components/UserDashboard.tsx
+++ src/Components/UserDashboard.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './UserDashboard.css';
 import PokemonCard from './PokemonCard.tsx';
-import { PokemonContext } from '../Context/Context';
+import { PokemonContext } from '../Context/Context.tsx';
 
 export default function UserDashboard() {
   const { googleUserData, userGoogleId } = useContext(PokemonContext)
@@ -31,19 +31,19 @@ export default function UserDashboard() {
     <div>
       <nav className='user-dashboard__container'>
         <h1 className='user-dashboard__title'>User Dashboard</h1>
-          <span onClick={handleGoToHome} className='user-dashboard__home-link'>Home</span>
-          <img className='user-dashboard__profile-img' src={profileImg} alt="user image" />
-          <span className='user-dashboard__information'>
+        <span onClick={handleGoToHome} className='user-dashboard__home-link'>Home</span>
+        <img className='user-dashboard__profile-img' src={profileImg} alt="user image" />
+        <span className='user-dashboard__information'>
           <span className='user-dashboard__user-name'>{userName}</span>
           <span className='user-dashboard__user-email'>{userEmail}</span>
         </span>
       </nav>
       <div className='cardList'>
-          {favoritePokemonCards && (
-            favoritePokemonCards.map((card) => 
+        {favoritePokemonCards && (
+          favoritePokemonCards.map((card) =>
             <PokemonCard img={card.images.small} name={card.name} cardId={card.id} key={card.id} hideDeleteButton={false} />
-            )
-          )}
+          )
+        )}
       </div>
     </div>
   )
