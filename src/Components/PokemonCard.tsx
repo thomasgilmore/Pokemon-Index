796e9f8543b14ce286533121e22c8d46abf677ef
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-30 14:37:21 -0400

    updated to typescript

--- src/Components/PokemonCard.tsx
+++ src/Components/PokemonCard.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import './PokemonCard.css';
-import { PokemonContext } from '../Context/Context';
+import { PokemonContext } from '../Context/Context.tsx';
 
 interface IPokemonCardProps {
   img: string;
@@ -14,11 +14,11 @@ export default function PokemonCard({ img, name, cardId, hideDeleteButton }: IPo
   const { handlePokemonCardChange, handlePokemonCardSave, handlePokemonCardDelete } = useContext(PokemonContext)
   return (
     <span className='pokemon-card-container'>
-    {hideDeleteButton ? 
-      <button className='pokemon-card__save-button' name={cardId} onClick={handlePokemonCardSave}>Save</button>
-    : <button className='pokemon-card__delete-button' name={cardId} onClick={handlePokemonCardDelete}>Delete</button>
-    }
-      <img  className="pokemon-card__img" src={img} alt={name} name={cardId} onClick={handlePokemonCardChange} />
+      {hideDeleteButton ?
+        <button className='pokemon-card__save-button' name={cardId} onClick={handlePokemonCardSave}>Save</button>
+        : <button className='pokemon-card__delete-button' name={cardId} onClick={handlePokemonCardDelete}>Delete</button>
+      }
+      <img className="pokemon-card__img" src={img} alt={name} name={cardId} onClick={handlePokemonCardChange} />
     </span>
   )
 }
