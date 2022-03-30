27ed059dea4c32b4a849a3f7c83b92e1b3a9a27c
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-30 14:32:31 -0400

    imported tsx files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import App from './App';
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
-import { PokemonContextProvider } from "./Context/Context";
+import { PokemonContextProvider } from "./Context/Context.tsx";
 
 ReactDOM.render(
   <BrowserRouter>
