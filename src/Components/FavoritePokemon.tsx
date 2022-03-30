23fb9bac17c432b5d584cefa470cfe954467f89a
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-03-30 14:35:42 -0400

    changed FavoritePokemon.js to typescript

--- src/Components/FavoritePokemon.tsx
+++ src/Components/FavoritePokemon.tsx
@@ -2,13 +2,13 @@ import React, { useContext } from 'react';
 import './FavoritePokemon.css';
 import { useNavigate } from 'react-router-dom';
 import PokemonCard from './PokemonCard.tsx';
-import { PokemonContext } from '../Context/Context';
+import { PokemonContext } from '../Context/Context.tsx';
 
 export default function FavoritePokemon() {
   const { cardList } = useContext(PokemonContext)
 
-  let secondAppStore = JSON.parse(localStorage.getItem("second-app-store"))
-  let favoritePokemonCards = secondAppStore.favoriteCards;
+  const secondAppStore = JSON.parse(localStorage.getItem("second-app-store"))
+  const favoritePokemonCards = secondAppStore.favoriteCards;
 
   const navigate = useNavigate();
 
@@ -26,14 +26,14 @@ export default function FavoritePokemon() {
         <span onClick={handleGoToHome} className='favorite-pokemon__header-home-link'>Home</span>
       </header>
       <div className='cardList'>
-          {/* {favoritePokemonCards.length > 0 ? favoritePokemonCards.map((card) => {
+        {/* {favoritePokemonCards.length > 0 ? favoritePokemonCards.map((card) => {
             return (<PokemonCard img={card.images.small} name={card.name} cardId={card.id} key={card.id} handlePokemonCardChange={handlePokemonCardChange} handlePokemonCardSave={handlePokemonCardSave} />)
           }) : null} */}
-          {favoritePokemonCards && (
-            favoritePokemonCards.map((card) => 
+        {favoritePokemonCards && (
+          favoritePokemonCards.map((card) =>
             <PokemonCard img={card.images.small} name={card.name} cardId={card.id} key={card.id} hideDeleteButton={false} />
-            )
-          )}
+          )
+        )}
       </div>
     </div>
   )
