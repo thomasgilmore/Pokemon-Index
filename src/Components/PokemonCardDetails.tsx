a069b1cfef6d6056f09ddefa779f7b3268735e5c
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-04-04 10:50:04 -0400

    imported types into PokemonCardDetails

--- src/Components/PokemonCardDetails.tsx
+++ src/Components/PokemonCardDetails.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './PokemonCardDetails.css';
+// Type
+import { IPokemonCardDetrailsProps } from '../Types/commonTypes';
 
-export default function PokemonCardDetails({ title, attacks }) {
+
+export default function PokemonCardDetails({ title, attacks }: IPokemonCardDetrailsProps) {
   return (
     <div className='pokemon-card-details__container'>
       <h1 className='pokemon-card-details__title'>{title}</h1>
